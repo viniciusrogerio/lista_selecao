2f595f7114af6b47591b61badf01cee14c05eb42
viniciusrogerio <50369912+viniciusrogerio@users.noreply.github.com>
2019-10-23 12:05:43 -0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,17 @@
+def informaCategoria(idade):
+	idade=int(idade)
+	if idade in range(0,2):
+		categoria = 'Bebê'
+	elif idade in range(2,13):
+		categoria = 'Criança'
+	elif idade in range(13,20):
+		categoria = 'Adolescente'
+	elif idade in range(20,61):
+		categoria = 'Adulto'
+	elif idade in range(61,80):
+		categoria = 'Adulto'
+	elif idade >= 80:
+		categoria = 'Longevo'
+	else:
+		categoria = 'Idade inválida!'
+	return categoria
